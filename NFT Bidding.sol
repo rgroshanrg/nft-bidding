556a92798da28c92b005c0401be328ba3edbd215
rgroshanrg <rgroshanrg@gmail.com>
2022-03-15 12:17:43 +0530

    nft mint function creation

--- NFT Bidding.sol
+++ NFT Bidding.sol
@@ -3,13 +3,20 @@
 pragma solidity ^0.8.7;
 
 contract NFTBidding {
-    struct nft {
+    struct NFT {
         uint id;
         string name;
         address owner;
         uint price;
     }
-    uint idCounter = 0;
-    mapping(uint => nft) nftMap;
-    
+    uint idCounter = 1;
+    mapping(uint => NFT) nftMap;
+
+    function mint(string memory _name, uint _price) public {
+        require(nftMap[idCounter].id == 0, "NFT with same id already exists");
+        NFT memory newNFT = NFT(idCounter, _name, msg.sender, _price);
+        nftMap[idCounter] = newNFT;
+        idCounter++;
+    }
+
 }
