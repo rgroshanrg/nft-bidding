a2e6dbb406a9acf83190b227e2b1bc2a1d450cc2
rgroshanrg <rgroshanrg@gmail.com>
2022-03-15 12:28:28 +0530

    nft mint function creation

--- NFT Bidding.sol
+++ NFT Bidding.sol
@@ -3,20 +3,34 @@
 pragma solidity ^0.8.7;
 
 contract NFTBidding {
+
+    // Structure body for NFT (details)
     struct NFT {
         uint id;
         string name;
         address owner;
         uint price;
     }
+
+    // For maintaing uniqueness of NFT, each time new id will be generated
     uint idCounter = 1;
+    
+    // For mapping id of each nft to its Structure Instance
     mapping(uint => NFT) nftMap;
 
+    // Function for minting nft 
     function mint(string memory _name, uint _price) public {
-        require(nftMap[idCounter].id == 0, "NFT with same id already exists");
-        NFT memory newNFT = NFT(idCounter, _name, msg.sender, _price);
-        nftMap[idCounter] = newNFT;
-        idCounter++;
+        require(nftMap[idCounter].id == 0, "NFT with same id already exists");  // checks if there already exist nft of same id
+        NFT memory newNFT = NFT(idCounter, _name, msg.sender, _price);  // creates new NFT using passes parameters and msg.sender
+        nftMap[idCounter] = newNFT;     // adds newly created NFT into nftMap
+        idCounter++;           // Increments idCounter for maintaining uniqueness
+    }
+
+    // Function for burning nft
+    function burn(uint _id) public {
+        require(nftMap[_id].id > 0, "NFT does not exists");     // Checks if NFT of given id exists or not
+        require(nftMap[_id].owner == msg.sender, "You are not owner of this NFT, you cannot burn it.");     // Check the owner of NFT is same as burner
+        delete nftMap[_id];     // Removes from the Map
     }
 
 }
